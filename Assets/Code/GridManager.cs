425819ff8dd84f4fe60800f859a0ad2dae1733a9
DannyGoblin <forgottenpinkie@icloud.com>
2025-09-05 13:30:22 -0700

    edits to GridManager

--- Assets/Code/GridManager.cs
+++ Assets/Code/GridManager.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class GridManager
+public class GridManager : MonoBehaviour
 {
+    //get vector location and xyz
+    public Transform gridPoint;
+    //get player location
+    public GameObject player;
 
+    void Start()
+    {
+        Vector3 positionPlayer = player.transform.position;
+        Vector3 positionPoint = gridPoint.transform.position;
+        
+        Debug.Log($"Target position: {positionPlayer.x} {positionPlayer.y} {positionPlayer.z}");
+    }
+    //use xyz with the player location/10
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, gridPoint.transform.position);
+        float cellDistance = distance / 10;
+    }
+    //create grid
+    //spawn objects on grid
 }
