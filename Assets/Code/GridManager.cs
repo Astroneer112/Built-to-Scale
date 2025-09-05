6dca27af81ab6915736c971096699971fde1ef03
DannyGoblin <forgottenpinkie@icloud.com>
2025-09-05 13:12:18 -0700

    Edited Code in GridManager

--- Assets/Code/GridManager.cs
+++ Assets/Code/GridManager.cs
@@ -2,37 +2,5 @@ using UnityEngine;
 
 public class GridManager
 {
-    //gameobject for the grid
-    public GameObject gridCellPrefab;
-    
-    public int gridSizeX = 10;
-    public int gridSizeY = 10;
-    public int gridSizeZ = 10;
 
-//Array to store grids
-    private GameObject[] gridCells;
-    public void Start()
-    {
-        GridCreation();
-    }
-    public void GridCreation()
-    {
-        gridCells = new GameObject(gridSizeX, gridSizeY, gridSizeZ);
-
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                for (int z = 0; z < gridSizeZ; z++)
-                {
-                    Vector3 position = new Vector3(x, y, z);
-
-                    GameObject cell = Instantiate(gridCellPrefab, position, Quaternion.identity);
-
-                    cell.name = $"Cell_{x}_{y}_{z}";
-                    gridCells[x,y,z] = cell;
-                }
-            }
-        }
-    }
 }
